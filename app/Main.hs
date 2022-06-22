5bcedd46a157f75dd2fd0d59651a454d3bc85b81
wafna <wafna@duck.com>
2022-06-22 10:49:57 -0700

    more stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import Data.Map
+
+data Sentence 
+    = Prop String
+    | Not Sentence
+    | And Sentence Sentence
+    | Or Sentence Sentence
+    deriving (Eq, Show, Read)
+
+type Valuation = Map String Bool
+
+evaluate :: Sentence -> Valuation -> Bool
+evaluate s v = case s of
+    Prop name -> v ! name
+    Not p -> not $ evaluate p v
+    And p q -> (evaluate p v) && (evaluate q v)
+    Or p q -> (evaluate p v) || (evaluate q v)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = 
+    do
+    putStrLn "Hello, Haskell!"
+    s <- return $ And (Prop "P") (Prop "Q")
+    putStrLn $ show s
+    putStrLn $ show $ evaluate s $ fromList [("P", True), ("Q", False)]
