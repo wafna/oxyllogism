34c6b4e9e0a08134328bce00b4ba46bbcc95f953
wafna <wafna@duck.com>
2022-06-23 09:14:34 -0700

    better examples plus comments

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -13,14 +13,15 @@ main =
         r = Prop "R"
     in
     do
-    putStrLn "Hello, Logic!"
+    putStrLn "Oxyllogism"
     x1 <- return $ p ∧ q
     putStrLn $ show x1
     putStrLn $ show $ evaluate x1 $ Map.fromList [("P", True), ("Q", False)]
-    putStrLn $ show $ evaluate x1 $ Map.fromList [("P", True), ("Q", True)]
     x2 <- return $ p ∧ (neg q)
     putStrLn $ show $ evaluate x2 $ Map.fromList [("P", True), ("Q", False)]
     putStrLn $ show $ substitute x2 "Q" $ r ∨ q
-    putStrLn $ show $ modusPonens (p ⊃ q) p 
-    putStrLn $ show $ modusPonens (x2 ⊃ q) x2 
+    putStrLn $ show $ modusPonens (p ⊃ q) p
+    putStrLn $ show $ modusPonens (x1 ⊃ x2) x1 
+    putStrLn $ show $ modusPonens (x2 ⊃ x1) x1 
+    -- If you use the negation operator you must section it.
     putStrLn $ show $ modusPonens ((¬) x2 ∨ q) x2
