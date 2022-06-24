875b0ad3ee4674ec55f3af0ff73baa69b594506c
wafna <wafna@duck.com>
2022-06-23 18:46:02 -0700

    tidy

--- src/main/Proof.hs
+++ src/main/Proof.hs
@@ -6,6 +6,7 @@ module Proof (
 
 import qualified Data.List as List
 import Control.Monad.State
+import Control.Monad.Except
 
 import Logic
 
@@ -32,34 +33,6 @@ data Step = Step { stepRule :: Rule, stepResult :: Sentence }
 data Derivation = Derivation { derivationGoal :: Sentence, derivationSteps :: [Step] }
     deriving (Show)
 
-spacedWords :: [String] -> String
-spacedWords ws = List.intercalate " " ws
-
--- | Pretty it up.
-showDerivation :: Derivation -> String
-showDerivation d = List.intercalate "\n" $ map (\ (nth, Step rule result) -> concat [show nth, ".  ", showRule rule, show result]) $ zip [(1::Int)..] $ reverse $ derivationSteps d
-
-showRule :: Rule -> String
-showRule rule = case rule of
-    Premise _ -> pad 20 "pr"
-    DoubleNegApply p -> pad 20 $ spacedWords [pad 8 "dn(a)", show (1 + p)]
-    DoubleNegRemove p -> pad 20 $ spacedWords [pad 8 "dn(r)", show (1 + p)]
-    SimplifyLeft p q -> pad 20 $ spacedWords [pad 8 "s(l)", show (1 + p), show (1 + q)]
-    SimplifyRight p q -> pad 20 $ spacedWords [pad 8 "s(r)", show (1 + p), show (1 + q)]
-    ModusPonens p q -> pad 20 $ spacedWords [pad 8 "mp", show (1 + p), show (1 + q)]
-    ModusTollens p q -> pad 20 $ spacedWords [pad 8 "mt", show (1 + p), show (1 + q)]
-    ModusTollendoPonensLeft p q -> pad 20 $ spacedWords [pad 8 "mtp(l)", show (1 + p), show (1 + q)]
-    ModusTollendoPonensRight p q -> pad 20 $ spacedWords [pad 8 "mtp(r)", show (1 + p), show (1 + q)]
-    Adjunction p q -> pad 20 $ spacedWords [pad 8 "adj", show (1 + p), show (1 + q)]
-    Addition p q -> pad 20 $ spacedWords [pad 8 "mtp(l)", show (1 + p), show q]
-
-pad :: Int -> String -> String
-pad n s = 
-    let len = length s in
-    if (len < n) 
-        then s ++ concat (take (n - len) $ repeat " ") 
-        else s
-
 -- Derivation state monad.
 type Derivator = State Derivation
 
@@ -67,16 +40,13 @@ type Derivator = State Derivation
 derive :: Sentence -> Derivator () -> Derivation
 derive goal actor = execState actor $ Derivation goal [] 
 
-currentStep :: Derivator Int
-currentStep = gets $ \ (Derivation _ steps) -> length steps - 1
-
 nthStep :: Int -> Derivator Step
 nthStep nth = gets $ \ d -> (reverse $ derivationSteps d) !! nth
 
 addStep :: Rule -> Sentence -> Derivator Int
 addStep rule result = do
     modify $ \ d -> d { derivationSteps = (Step rule result) : derivationSteps d }
-    currentStep
+    gets $ \ (Derivation _ steps) -> length steps - 1
 
 -- | asserts that the result of the given step satisfies the goal of the derivation.
 qed :: Int -> Derivator ()
@@ -84,7 +54,7 @@ qed i = do
     r <- nthStep i
     g <- gets derivationGoal
     if stepResult r /= g
-        then error $ "Goal not reached: found " ++ show r ++ " need " ++ show g
+        then error $ concat ["Goal not reached: found ", show r, " need ", show g]
         else return ()
 
 checkResult :: Sentence -> Sentence -> Rule -> Derivator Int
@@ -98,6 +68,8 @@ checkMaybeResult expected actual rule =
     case actual of
         Nothing -> error $ concat ["Invalid application of ", showRule rule, ": expected ", show expected, ", got ", show actual]
         Just u -> checkResult expected u rule
+
+-- Rules
 
 -- | Introduce a premise, return the step number.
 pr :: Sentence -> Derivator Int
@@ -167,3 +139,34 @@ add :: Int -> Sentence -> Sentence -> Derivator Int
 add p q r = do
     x <- nthStep p
     checkResult r (addition (stepResult x) q) (Addition p q)
+
+-- Display.
+
+spacedWords :: [String] -> String
+spacedWords ws = List.intercalate " " ws
+
+-- | Pretty it up.
+showDerivation :: Derivation -> String
+showDerivation d = List.intercalate "\n" $ map (\ (nth, Step rule result) -> concat [show nth, ".  ", showRule rule, show result]) $ zip [(1::Int)..] $ reverse $ derivationSteps d
+
+showRule :: Rule -> String
+showRule rule = case rule of
+    Premise _ -> pad 20 "pr"
+    DoubleNegApply p -> pad 20 $ spacedWords [pad 8 "dn(a)", show (1 + p)]
+    DoubleNegRemove p -> pad 20 $ spacedWords [pad 8 "dn(r)", show (1 + p)]
+    SimplifyLeft p q -> pad 20 $ spacedWords [pad 8 "s(l)", show (1 + p), show (1 + q)]
+    SimplifyRight p q -> pad 20 $ spacedWords [pad 8 "s(r)", show (1 + p), show (1 + q)]
+    ModusPonens p q -> pad 20 $ spacedWords [pad 8 "mp", show (1 + p), show (1 + q)]
+    ModusTollens p q -> pad 20 $ spacedWords [pad 8 "mt", show (1 + p), show (1 + q)]
+    ModusTollendoPonensLeft p q -> pad 20 $ spacedWords [pad 8 "mtp(l)", show (1 + p), show (1 + q)]
+    ModusTollendoPonensRight p q -> pad 20 $ spacedWords [pad 8 "mtp(r)", show (1 + p), show (1 + q)]
+    Adjunction p q -> pad 20 $ spacedWords [pad 8 "adj", show (1 + p), show (1 + q)]
+    Addition p q -> pad 20 $ spacedWords [pad 8 "mtp(l)", show (1 + p), show q]
+
+pad :: Int -> String -> String
+pad n s = 
+    let len = length s in
+    if (len < n) 
+        then s ++ concat (take (n - len) $ repeat " ") 
+        else s
+
