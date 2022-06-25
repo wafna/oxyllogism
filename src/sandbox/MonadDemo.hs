856d05e57770274e9cf73fd34d99862cb78646b5
wafna <wafna@duck.com>
2022-06-25 13:30:20 -0700

    io flip state and except

--- src/sandbox/MonadDemo.hs
+++ src/sandbox/MonadDemo.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE RankNTypes #-}
 module Main (main) where
 
+--------------------------------------------------------------------------------------------------
 -- Demonstrating the stacking of monads.
 -- Note that the two test functions can be used in either stack due to their flexibly constrained definitions.
 -- https://stackoverflow.com/questions/31838776/whats-the-difference-between-statet-s-exceptt-e-m-and-exceptt-e-statet-s-m
@@ -29,29 +30,46 @@ runStateExceptT s = flip runStateT s . runExceptT
 runExceptStateT :: Monad m => s -> StateT s (ExceptT e m) a -> m (Either e (a, s))
 runExceptStateT s = runExceptT . flip runStateT s
 
+--------------------------------------------------------------------------------------------------
 -- Example of IO with monad transformers.
 
 type MyState = Int
 type MyError = String
-type MyMonad a = ExceptT MyError (StateT MyState IO) a
 
--- runMyMonad initState action = liftIO $ runExceptT $ runStateT action initState
-runMyMonad :: MyState -> MyMonad a -> IO (Either MyError a, MyState)
-runMyMonad initState action = liftIO $ runStateT (runExceptT action) initState
+type MyMonadSE a = ExceptT MyError (StateT MyState IO) a
+
+-- runMyMonadSE initState action = liftIO $ runExceptT $ runStateT action initState
+runMyMonadSE :: MyState -> MyMonadSE a -> IO (Either MyError a, MyState)
+runMyMonadSE initState action = liftIO $ runStateT (runExceptT action) initState
+
+type MyMonadES a = StateT MyState (ExceptT MyError IO) a
+
+runMyMonadES :: MyState -> MyMonadES a -> IO (Either MyError (a, MyState))
+runMyMonadES initState action = liftIO $ runExceptT $ runStateT action initState
+
 
 -- Most general type signature.
+-- Works with either monad.
 myf1 :: (MonadState MyState m, MonadError MyError m, MonadIO m) => m String
 myf1 = do
     get >>= liftIO . print
-    return "F2!"
+    return "Works in either monad."
 
-myf2 :: MyMonad String
-myf2 = do
+mySE_1 :: MyMonadSE String
+mySE_1 = do
     get >>= liftIO . print
     return "F1!"
 
-myf3 :: MyMonad ()
-myf3 = throwError "F3!"
+mySE_2 :: MyMonadSE ()
+mySE_2 = throwError "F3!"
+
+myES_1 :: MyMonadES String
+myES_1 = do
+    get >>= liftIO . print
+    return "F1!"
+
+myES_2 :: MyMonadES ()
+myES_2 = throwError "F3!"
 
 main :: IO ()
 main = do
@@ -61,10 +79,14 @@ main = do
     print $ runIdentity . runStateExceptT 3 $ test2
     print $ runIdentity . runExceptStateT 3 $ test2
     -- Some stuff with IO
-    (runMyMonad 42 $ do
+    (runMyMonadSE 42 $ do
             liftIO $ putStrLn "woohoo"
             modify $ (+) 86)
         >>= print
-    runMyMonad 42 myf1 >>= print
-    runMyMonad 42 myf2 >>= print    
-    runMyMonad 42 myf3 >>= print    
+    putStrLn "IO Monads..."
+    runMyMonadSE 42 myf1 >>= print
+    runMyMonadSE 42 mySE_1 >>= print    
+    runMyMonadSE 42 mySE_2 >>= print    
+    runMyMonadES 42 myf1 >>= print    
+    runMyMonadES 42 myES_1 >>= print    
+    runMyMonadES 42 myES_2 >>= print    
